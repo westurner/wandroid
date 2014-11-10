394ec1f973cc9817b7c6e7b4e2724822dfeab475
Wes Turner <50891+westurner@users.noreply.github.com>
2014-11-10 03:47:07 -0600

    BLD,BUG: setup_adt_sdk.sh

--- scripts/setup_adt_sdk.sh
+++ scripts/setup_adt_sdk.sh
@@ -1,19 +1,45 @@
-#!/bin/sh
+#!/bin/bash -x
 # Download Android ADT SDK
 # -------------------------
 # https://developer.android.com/sdk/index.html
 
-version="20131030"
-vername="adt-bundle-linux-x86-$version"
-#vername="adt-bundle-linux-x86_64-$version"
-archive="$version.zip"
-android="/opt/android"
+version="20140702"
+prefix="${HOME}/.local/android"  # /opt/android"
 
-sudo mkdir -p $android
-sudo chown $USER:$USER $android
-cd $android
+UNAME=$(uname)
+ARCH=$(uname -m)
 
-curl -fSL 'http://dl.google.com/android/adt/$archive' -O
-unzip $archive -d
-ln -s $android/$vername $android/adt
+if [ ${UNAME} == "Darwin" ]; then
+    platformstr="mac"
+    chownstr=$USER
+    md5cmd="md5"
+    browser="open"
+elif [ ${UNAME} == "Linux" ]; then
+    platformstr="linux"
+    chownstr=$USER:$USER
+    md5cmd="md5sum"
+    browser="x-www-browser"
+else
+    echo "Unsupported platform: ${UNAME}"
+    exit 1
+fi
+
+vername="adt-bundle-${platformstr}-${ARCH}-${version}"
+archivename="${vername}.zip"
+archiveurl="http://dl.google.com/android/adt/${archivename}"
+destpath="${prefix}/${vername}"
+adtprefix=${prefix}/adt
+
+sudo mkdir -p ${prefix}
+sudo chown ${chownstr} ${prefix}
+(cd ${prefix} && \
+    curl -fSL -C - "${archiveurl}" -o "${archivename}" && \
+    ${md5cmd} "${archivename}" && \
+    unzip -q ${archivename} -d ${prefix}/)
+
+ln -s ${destpath}/ ${adtprefix}
+
+echo ${browser} https://developer.android.com/sdk/installing/adding-packages.html &
+
+${adtprefix}/sdk/tools/android update sdk &
 
